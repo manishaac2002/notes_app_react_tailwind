0f499ead9ee01149fd9785e26c40c1e93b7ffa69
manishaac2002 <manishaac2002@gmail.com>
2024-08-13 16:53:33 +0530

    feat:chore--

--- src/components/main/TextComponent.jsx
+++ src/components/main/TextComponent.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
 const TextComponent = () => {
+
+const [todoString,setTodoString]=React.useState('')
+  const handleClick=(e)=>{
+    e.preventDefault()
+  }
+
   return (
     <div className='grid grid-cols-2 m-2 gap-5 '>
-        <div className='border-r-2'>TextComponent</div>
+        <div className='border border-black'>
+         <form className='' onSubmit={handleClick}>
+          <input className="text-gray-400 border p-2 rounded" type="text" placeholder='Enter Notes' 
+          onChange={(e)=>setTodoString(e.target.value)} />
+         </form>
+        </div>
         <div className='border border-black '>TextComponent</div>
     </div>
   )
